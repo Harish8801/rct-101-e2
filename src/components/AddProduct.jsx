6133f77747694bc1f7abc3d63d202cf1cca56873
Harish8801 <92293513+Harish8801@users.noreply.github.com>
2022-05-30 17:17:29 +0530

    up

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -12,22 +12,24 @@ const AddProduct = () => {
 
   return (
     <>
-      <Button my={4} data-cy="add-product-button"></Button>
+      <button data-cy='add-product-button'>
+        Add new Product
+      </button>
       <Modal>
         <ModalBody pb={6}>
-          <Input data-cy="add-product-title" />
-          <Select data-cy="add-product-category">
-            <option data-cy="add-product-category-shirt"></option>
-            <option data-cy="add-product-category-pant"></option>
-            <option data-cy="add-product-category-jeans"></option>
+          <Input data-cy='add-product-title' />
+          <Select data-cy='add-product-category'>
+            <option data-cy='add-product-category-shirt'>Shirt</option>
+            <option data-cy='add-product-category-pant'>Pant</option>
+            <option data-cy='add-product-category-jeans'>Jeans</option>
           </Select>
-          <RadioGroup data-cy="add-product-gender">
-            <Radio data-cy="add-product-gender-male"></Radio>
-            <Radio data-cy="add-product-gender-female"></Radio>
-            <Radio data-cy="add-product-gender-unisex"></Radio>
+          <RadioGroup data-cy='add-product-gender'>
+            <Radio data-cy='add-product-gender-male'>Male</Radio>
+            <Radio data-cy='add-product-gender-female'>Female</Radio>
+            <Radio data-cy='add-product-gender-unisex'>unisex</Radio>
           </RadioGroup>
-          <Input data-cy="add-product-price" />
-          <Button data-cy="add-product-submit-button"></Button>
+          <Input data-cy='add-product-price' />
+          <Button data-cy='add-product-submit-button'></Button>
         </ModalBody>
       </Modal>
     </>
